5f897ab499ad3a7f225356c0bf77712d8cd9b5a4
Akshay Laxmipathi <52963912+Akshay-Laxmipathi@users.noreply.github.com>
2024-09-20 16:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-let data = document.getElementById("bands");
+let data = document.getElementById("band");
 
 bands.sort((a,b) => {
 	let x = "", y = "";
@@ -28,5 +28,5 @@ bands.sort((a,b) => {
 for(let i=0; i<bands.length; i++) {
 	let element = document.createElement("li");
 	element.innerText = bands[i];
-	band.appendChild(element);
+	data.appendChild(element);
 }
